d9cb2b93d0160accbf1117bd79f1864b5b49787f
Zhao Tang <tangzhao20@gmail.com>
2024-09-13 11:56:56 -0500

    Modified shared/linear_solver.f90

--- shared/linear_solver.f90
+++ shared/linear_solver.f90
@@ -3,19 +3,19 @@ subroutine dlinear_solver(n, m, A, B, X, inode, verbose, errinfo)
   use myconstants
   implicit none
 
-  integer, intent(in) :: n, m, inode
-  real(dp),intent(in) :: A(n,n)
-  real(dp),intent(in) :: B(n,m)
-  real(dp),intent(out) :: X(n,m)
-  integer, intent(in) :: errinfo ! issue: why intent(in)?
+  integer, intent (in) :: n, m, inode
+  real (dp), intent (in) :: A(n,n)
+  real (dp), intent (in) :: B(n,m)
+  real (dp), intent (out) :: X(n,m)
+  integer, intent (in) :: errinfo ! issue: why intent(in)?
 
-  logical   :: verbose
+  logical :: verbose
   ! Temporary local variables
-  real(dp)  :: Af(n,n), r(n), c(n), ferr(m), berr(m), work(4*n)
-  integer   :: ipiv(n), iwork(n)
+  real (dp) :: Af(n,n), r(n), c(n), ferr(m), berr(m), work(4*n)
+  integer :: ipiv(n), iwork(n)
   character :: equed, fact, trans
-  integer   :: lda, ldaf
-  real(dp)  :: rcond
+  integer :: lda, ldaf
+  real (dp) :: rcond
   
   fact = 'E'
   trans = 'N'
@@ -23,25 +23,28 @@ subroutine dlinear_solver(n, m, A, B, X, inode, verbose, errinfo)
   !           af, ldaf, ipiv, equed, r, c,
   !           b, ldb, x, ldx, rcond, ferr,
   !           berr, work, iwork, info )
-  if(verbose) write (*,'("Node",i6," call dgesvx to solve A*X = B")') inode
-  call dgesvx(fact, trans, n, m, A, n, &
-    Af, n, ipiv, equed, r, c, &
-    B, n, X, n, rcond, ferr, &
-    berr, work, iwork, errinfo)
-  if(errinfo .eq. 0) then
-     if(verbose) write(*,'(" dgesvx is successfully excecuted. ")')
-  elseif (errinfo .lt. 0) then
-     write(*,'(" Node",i6," The ", i2, "-th parameter of dgesvx has an illegal value.")') &
-       inode, -errinfo
-  elseif (errinfo .gt. 0 .and. errinfo .le. n) then 
-     write(*,'(" Node",i6," The U matrix is singular. LU factorization cannot be", &
-       "completed.")') inode
+  if (verbose) write (*, '(a,i6,a)') "Node", inode, &
+      " call dgesvx to solve A*X = B"
+  call dgesvx(fact, trans, n, m, A, n, Af, n, ipiv, equed, r, c, B, n, X, n, &
+      rcond, ferr, berr, work, iwork, errinfo)
+  if (errinfo == 0) then
+    if (verbose) print *," dgesvx is successfully excecuted. "
+  else if (errinfo < 0) then
+    write (*, '(a,i6,a,i2,a)') " Node", inode, " The ", -errinfo, &
+         "-th parameter of dgesvx has an illegal value."
+  else if (errinfo > 0 .and. errinfo <= n) then
+    write (*, '(a,i6,a)') " Node", inode, &
+        " The U matrix is singular. LU factorization cannot be completed."
+
   else ! errinfo = n+1
-     write(*,'(" Node",i6," THe U matrix is nonsingular, but rcond is less than machine", &
-       "precision.")') inode
-     write(*,'(" rcond is the reciprocal of condition number: = ", f12.6)') &
-       rcond
-  endif
+    if (verbose) then 
+      write (*, '(a,i6,a,a)') " Node", inode, &
+          " The U matrix is nonsingular, but rcond is less than ", &
+          "machine precision."
+      write (*, '(a,a,f12.6)') " rcond is the reciprocal of condition ", &
+          "number: = ", rcond
+    end if
+  end if
 
   return
 end subroutine dlinear_solver
