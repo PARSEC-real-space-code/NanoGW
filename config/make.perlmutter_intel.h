cdc12569ca294fe1c0061d1c9a28213643a6d1f9
Zhao Tang <tangzhao20@gmail.com>
2024-10-10 10:38:21 -0700

    Modified perlmutter config files

--- config/make.perlmutter_intel.h
+++ config/make.perlmutter_intel.h
@@ -1,29 +1,30 @@
 ##
-# Ubuntu Linux, gfortran Fortran compiler, FFTW 3.*, MPI
 #
+# NERSC Perlmutter, Intel compiler, FFTW 3, MPI
+#
+# module load cpu intel/2023.2.0 cray-hdf5-parallel/1.12.2.9
+#
+##
+
 FCPP    = /usr/bin/cpp -P -traditional-cpp
-CPPOPT  = -DUSEFFTW3 -DMPI # -DDEBUG 
+CPPOPT  = -DUSEFFTW3 -DMPI # -DDEBUG
 
 EXT     = .intel.mpi
 
 F90s    = ftn
 F90     = $(F90s)
 
-#FFTW3_INC_DIR = 
-#FFTW3_LIB_DIR = 
-HDF5_INC_DIR  = /opt/cray/pe/hdf5/1.12.2.3/intel/19.0/include 
-HDF5_LIB_DIR  = /opt/cray/pe/hdf5/1.12.2.3/intel/19.0/lib
-EXTRA_DIR     = /global/u1/w/weiwei/src/new-nanogw/nanogw-2023_8_23/LIB_intel
-LAPACK_DIR    = 
+HDF5_DIR   = /opt/cray/pe/hdf5/1.12.2.9/intel/2023.2
+HDF5_INC   = ${HDF5_DIR}/include
+HDF5_LIB   = ${HDF5_DIR}/lib
+EXTRA_DIR  = ../LIB
 
-OPTS       = -O2 # -check all
-OPTS2      = -I${HDF5_INC_DIR} 
+OPTS       = -O3 -qmkl=cluster # -check all
+OPTS2      = -I${HDF5_INC}
 
-LIBLAPACK  = -qmkl="cluster" 
+LIBXC      = -L${EXTRA_DIR}/libstring_f/lib -L${EXTRA_DIR}/libxc/lib -lxc -lstring_f
 
-LIBXC      = -L${EXTRA_DIR}/libstring_f/lib -L${EXTRA_DIR}/libxc/lib -lxc -lstring_f 
-
-LIBHDF5    = -L${HDF5_LIB_DIR} -lhdf5_fortran_intel -lhdf5_fortran
+LIBHDF5    = -L${HDF5_LIB} -lhdf5_fortran_intel -lhdf5_fortran
 
 AUX_SRC = aux_generic.f90
 
