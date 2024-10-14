4ef40dbf44dc3dbc7d91e2166faf0351a666bc8a
Zhao Tang <tangzhao20@gmail.com>
2024-10-14 14:22:08 -0700

    Updated make.perlmutter_nvidia.h

--- config/make.perlmutter_nvidia.h
+++ config/make.perlmutter_nvidia.h
@@ -19,7 +19,7 @@ HDF5_INC   = ${HDF5_DIR}/include
 HDF5_LIB   = ${HDF5_DIR}/lib
 EXTRA_DIR  = ../LIB
 
-OPTS       = -O3 -cuda -cudalib=cublas # -Mbounds -Mchkptr -g -traceback
+OPTS       = -fast -cuda -cudalib=cublas -gpu=cc80 # -Mbounds -Mchkptr -g -traceback
 OPTS2      = -I${HDF5_INC}
 
 LIBXC      = -L${EXTRA_DIR}/libstring_f/lib -L${EXTRA_DIR}/libxc/lib -lxc -lstring_f
