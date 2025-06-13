e7ef6a138f95ad4e30299c6b9d8967c37f7d42ab
Zhao Tang <tangzhao20@gmail.com>
2025-06-13 13:10:30 -0700

    Update the config file for Perlmutter Nvidia

--- config/make.perlmutter_nvidia.h
+++ config/make.perlmutter_nvidia.h
@@ -2,19 +2,18 @@
 #
 # NERSC Perlmutter, Nvidia compiler, FFTW 3, MPI, CUDA
 #
-# module load nvhpc/24.5 cray-hdf5-parallel/1.12.2.9 cray-fftw/3.3.10.6
+# module load PrgEnv-nvidia/8.5.0 cray-hdf5-parallel/1.14.3.1 cray-fftw/3.3.10.8
 #
 ##
 
 FCPP    = /usr/bin/cpp -P -traditional-cpp
-CPPOPT  = -DUSEFFTW3 -DMPI -D_CUDA # -DDEBUG
+CPPOPT  = -DUSEFFTW3 -DMPI -D_CUDA  # -DDEBUG
 
 EXT     = .nvidia.mpi
 
 F90s    = ftn
 F90     = $(F90s)
 
-HDF5_DIR   = /opt/cray/pe/hdf5/1.12.2.9/nvidia/23.3
 HDF5_INC   = ${HDF5_DIR}/include
 HDF5_LIB   = ${HDF5_DIR}/lib
 EXTRA_DIR  = ../LIB
@@ -26,5 +25,5 @@ LIBXC      = -L${EXTRA_DIR}/libstring_f/lib -L${EXTRA_DIR}/libxc/lib -lxc -lstri
 
 LIBHDF5    = -L${HDF5_LIB} -lhdf5_fortran_nvidia -lhdf5_fortran
 
-AUX_SRC = aux_generic.f90
+AUX_SRC    = aux_generic.f90
 
